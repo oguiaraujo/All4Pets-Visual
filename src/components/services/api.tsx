1f5365f7222aa205730f030816e19d5102913e24
Guilherme Araújo <guilherme.araujo.702@ufrn.edu.br>
2026-02-03 15:36:50 -0300

    fix(services): corrige erro de rota

--- src/components/services/api.tsx
+++ src/components/services/api.tsx
@@ -1,5 +1,5 @@
 // ALTERE conforme os endpoints do seu amigo
-const API_URL = 'http://127.0.0.1:8000/'; // URL do Django
+const API_BASE = process.env.NEXT_PUBLIC_API_BASE_URL; // URL do Django
 
 export interface Service {
   id: number;
@@ -13,14 +13,14 @@ export interface Service {
 export const servicesApi = {
   // GET todos os serviços
   getAll: async (): Promise<Service[]> => {
-    const response = await fetch(`${API_URL}/services/`);
+    const response = await fetch(`${API_BASE}/services/`);
     if (!response.ok) throw new Error('Erro ao buscar serviços');
     return response.json();
   },
 
   // GET um serviço
   getById: async (id: number): Promise<Service> => {
-    const response = await fetch(`${API_URL}/services/${id}/`);
+    const response = await fetch(`${API_BASE}/services/${id}/`);
     return response.json();
   },
 
@@ -28,7 +28,7 @@ export const servicesApi = {
   create: async (data: any): Promise<Service> => {
   console.log('Enviando para API:', data);
   
-  const response = await fetch(`${API_URL}/services/`, {
+  const response = await fetch(`${API_BASE}/services/`, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
@@ -49,7 +49,7 @@ export const servicesApi = {
 
   // PUT atualizar serviço
   update: async (id: number, data: Partial<Service>): Promise<Service> => {
-    const response = await fetch(`${API_URL}/services/${id}/`, {
+    const response = await fetch(`${API_BASE}/services/${id}/`, {
       method: 'PUT',
       headers: {
         'Content-Type': 'application/json',
@@ -62,7 +62,7 @@ export const servicesApi = {
 
   // DELETE serviço
   delete: async (id: number): Promise<boolean> => {
-    const response = await fetch(`${API_URL}/services/${id}/`, {
+    const response = await fetch(`${API_BASE}/services/${id}/`, {
       method: 'DELETE',
     });
     return response.ok;
